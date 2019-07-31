c56075c6300cbd592ad04cb33fae036e4795d08b
hyerim.kwon <hyerim.kwon@B106-18.soit.local>
2019-07-31 15:26:20 +1200

    Added servlet to check login credentials

--- BudgetingApp/src/myServlets/LoginCheck.java
+++ BudgetingApp/src/myServlets/LoginCheck.java
@@ -34,8 +34,14 @@ public class LoginCheck extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		String username = request.getParameter("uname");
+		String password = request.getParameter("password");
+		if( username.equals("admin")&& password.equals("admin")) {
+			response.sendRedirect("Home.jsp");
+		}else {
+			response.sendRedirect("Error.jsp");
+		}
 	}
 
 }
