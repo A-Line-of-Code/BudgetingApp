a987de664b2c70d030ff3fd3684f3c04f41b87e0
hyerim.kwon <hyerim.kwon@B106-18.soit.local>
2019-08-07 15:19:49 +1200

    Deleted unused classes and fixed database error

--- BudgetingApp/src/Model/Expenditure.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-public class Expenditure {
-	private String date;
-	private String day;
-	private String item;
-	private String cost;
-}
--- BudgetingApp/src/logic/BusinessLogic.java
+++
@@ -1,5 +0,0 @@
-package logic;
-
-public class BusinessLogic {
-	//code the business logic of your web app
-}
